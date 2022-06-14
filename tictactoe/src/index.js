ba52695304666c421641a3709727d9e255bb74c6
Gabriel Zecchi <g.zecchi@hotmail.com>
2022-06-13 22:17:05 -0300

    Commit final

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -48,12 +48,13 @@ class Game extends React.Component {
             history:[{
                 squares:Array(9).fill(null),
             }],
+            stepNumber:0,
             xIsNext: true,
         }
     }
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
         if(winner){
@@ -61,6 +62,18 @@ class Game extends React.Component {
         }else{
             status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
         }
+
+        const moves = history.map((step,move)=>{
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        })
+
         return (
             <div className="game">
                 <div className="game-board">
@@ -71,16 +84,16 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = this.state.squares.slice();
+        const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
             return;
         }
@@ -89,7 +102,15 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step%2) === 0,
         });
     }
 }
