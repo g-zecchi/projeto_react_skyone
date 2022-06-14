c76e951822edf6d0dde87d242fe7eb8dc5a88052
Gabriel Zecchi <g.zecchi@hotmail.com>
2022-06-13 21:58:53 -0300

    Passando método handleClick para game

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -12,17 +12,7 @@ function Square(props) {
 
 class Board extends React.Component {
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
+    
 
     renderSquare(i){
         return (<Square value={this.props.squares[i]}
@@ -30,16 +20,7 @@ class Board extends React.Component {
         />);
     }
     render(){
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = 'Winner: ' + winner;
-        }else{
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return ( <div>
-            <div className="status">{status}</div>
             <div className="board-row">
                 {this.renderSquare(0)}
                 {this.renderSquare(1)}
@@ -94,6 +75,22 @@ class Game extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
     }
 }
 
